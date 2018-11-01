162e6dfb5c7c9397ac0d61c7f855aa6a773a2726
Jan200101 <sentrycraft123@gmail.com>
2018-11-01 04:34:41 +0100

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,13 @@
 
 int main()
 {
-    // replace this
+    // init vector with Schuler class
     std::vector<Schuler> schulerliste(5);
 
+    // read res/test.csv into schulerliste
     readSchuler(schulerliste);
 
+    // write schulerliste into res/test.csv
     writeSchuler(schulerliste);
 
     std::cout << "DONE" << std::endl;
